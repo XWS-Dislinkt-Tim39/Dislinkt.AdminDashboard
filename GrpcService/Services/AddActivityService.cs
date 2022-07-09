b0a7717b97c45964d59b06ccae82c3689763740b
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-07-10 00:35:30 +0200

    add grpc service to add activity

---
+++ GrpcService/Services/AddActivityService.cs
@@ -0,0 +1,74 @@
+﻿using Dislinkt.AdminDashboard.Data;
+using Dislinkt.AdminDashboard.Domain;
+using Dislinkt.AdminDashboard.Interfaces.Repositories;
+using Grpc.Core;
+using GrpcAddActivityService;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Type = Dislinkt.AdminDashboard.Domain.Type;
+
+namespace GrpcService.Services
+{
+    public class AddActivityService : addActivityGreeter.addActivityGreeterBase
+    {
+        private IActivityRepository _activityRepository;
+
+        public AddActivityService(IActivityRepository activityRepository)
+        {
+
+            _activityRepository = activityRepository;
+        }
+
+        public override async Task<ActivityReply> addActivity(ActivityRequest request, ServerCallContext context)
+        {
+            bool result;
+            Type t = Type.Post;
+            if (request.Type == "Post")
+            {
+                t = Dislinkt.AdminDashboard.Domain.Type.Post;
+            }
+            else if (request.Type == "Job")
+            {
+                t = Type.Job;
+            }
+            else if (request.Type == "Connection")
+            {
+                t =Type.Connection;
+            }
+            else if (request.Type == "Registration")
+            {
+                t =Type.Registration;
+            }
+            try
+            {
+                var activity = new NewActivityData
+                {
+                    UserId = System.Guid.Parse(request.UserId),
+                    Text = request.Text,
+                    Date = DateTime.ParseExact(request.Date, "yyyy-MM-dd HH:mm tt", null),
+                Type = t
+                };
+
+                await _activityRepository.CreateActivity(activity);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return await Task.FromResult(new ActivityReply
+                {
+                    Successful = false,
+                    Message = $"{ex}"
+                });
+            }
+            return await Task.FromResult(new ActivityReply
+            {
+                Successful = result,
+                Message = $"Notifikacija: {request.UserId} | {request.Text} | {request.Type} | {request.Date}"
+            });
+        }
+           
+        
+    }
+}
